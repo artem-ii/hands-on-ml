a8e2dc7ded1227c1ea761b867e6b9ee1fc0100e9
Artemii Nikitin <artemii.a.nikitin@gmail.com>
2022-10-06 21:04:57 +0200

    Until Error Analysis

--- hands_on_ml_mnist.py
+++ hands_on_ml_mnist.py
@@ -315,3 +315,172 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     
 plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
 plt.show()
+
+threshold_90_precision = thresholds[np.argmax(precisions >= 0.9)]
+y_train_pred_90 = (y_scores >= threshold_90_precision)
+precision_score(y_train_5, y_train_pred_90)
+# Out[35]: 0.9000345901072293
+
+recall_score(y_train_5, y_train_pred_90)
+# Out[36]: 0.4799852425751706
+
+# %%%% ROC Curve
+
+'''
+
+Receiver operating characteristic curve. Plots true positive rate (TPR
+or recall) against false positive rate (FPR) — ratio of negative instances
+incorrectly classified as positive. 
+
+'''
+from sklearn.metrics import roc_curve
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    
+plot_roc_curve(fpr, tpr)
+plt.show()
+
+# TODO Finish plots
+
+from sklearn.metrics import roc_auc_score
+roc_auc_score(y_train_5, y_scores)
+# Out[42]: 0.9604938554008616
+
+'''
+
+A rule of a thumb is to use PR curve when positive class is rare or when you
+care more about the false positives than false negatives. Otherwise use ROC.
+
+In this example ROC gives very high score, but this is due to relatively
+small number of positive vs negative instances in the dataset.
+
+'''
+
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method="predict_proba")
+'''
+
+In Random Forest there's "predict proba" instead of "decision_function"
+in SGDClassifier. Predict proba gives a row per instance and
+a column per class - array of probabilities
+(that given instance belongs to given class)
+
+Use positive class probability as a score
+
+'''
+
+y_scores_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5,
+                                                      y_scores_forest)
+
+plt.plot(fpr, tpr, "b:", label="SGD")
+plot_roc_curve(fpr_forest, tpr_forest, "Random forest")
+plt.legend(loc="lower right")
+plt.show()
+
+roc_auc_score(y_train_5, y_scores_forest)
+# Out[47]: 0.9983436731328145
+
+# %% Multiclass Classification
+# Or multinomial classifiers
+
+'''
+
+It's possible to train 10 binary classifiers to classify numbers
+into ten classes — One-versus-rest (OvR) strategy (or one vs all).
+Decision is made based on choosing a classifier with the highest score.
+
+OvO - one-versus-one strategy is to train binary classifiers
+on pairs of numbers "0 or 1" etc.
+With N classes, N * (N - 1) / 2 classifiers will be needed.
+
+OvO is preferred for algorithms which scale poorlyto large datasets like SVM
+For other binary classifiers usually OvR is preferred.
+
+'''
+
+from sklearn.svm import SVC
+
+svm_clf = SVC()
+svm_clf.fit(X_train, y_train)
+svm_clf.predict([some_digit])
+
+# Out[48]: array([5], dtype=int8) - Correct prediction
+
+some_digit_scores = svm_clf.decision_function([some_digit])
+some_digit_scores
+
+'''
+array([[ 1.72501977,  2.72809088,  7.2510018 ,  8.3076379 , -0.31087254,
+         9.3132482 ,  1.70975103,  2.76765202,  6.23049537,  4.84771048]])
+         ^^^^^^^^^
+Scikit learn automatically uses OvO approach, and the score for 5 wins.
+
+Didn't get it, there should be 45 classifiers in OvO right?
+
+'''
+
+np.argmax(some_digit_scores)
+# Out[50]: 5
+
+svm_clf.classes_
+# Out[51]: array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=int8)
+
+from sklearn.multiclass import OneVsRestClassifier
+
+ovr_clf = OneVsRestClassifier(SVC()) # fit very slow
+ovr_clf.fit(X_train, y_train)
+
+ovr_clf.predict([some_digit])
+# Out[54]: array([5], dtype=int8)
+
+# Train a multiple SGD classifier
+sgd_clf.fit(X_train, y_train)
+sgd_clf.predict([some_digit])
+# Out[57]: array([3], dtype=int8) - wrong!
+
+sgd_clf.decision_function([some_digit])
+
+'''
+
+Out[58]: 
+array([[-31893.03095419, -34419.69069632,  -9530.63950739,
+          1823.73154031, -22320.14822878,  -1385.80478895,
+        -26188.91070951, -16147.51323997,  -4604.35491274,
+        -12050.767298  ]])
+
+'''
+
+cross_val_score(sgd_clf, X_train, y_train,
+                cv=3, scoring="accuracy")
+# Out[59]: array([0.87365, 0.85835, 0.8689 ])
+
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
+cross_val_score(sgd_clf, X_train_scaled, y_train,
+                cv=3, scoring="accuracy")
+# Out[64]: array([0.8983, 0.891 , 0.9018])
+
+
+# Train a multiple SGD classifier now on scaled data
+# Also trying multithreading
+
+from joblib import parallel_backend
+
+with parallel_backend('threading', n_jobs=8):
+    sgd_clf.fit(X_train_scaled, y_train)
+
+sgd_clf.predict([some_digit])
+
+# Out[68]: array([3], dtype=int8) - still wrong
+
